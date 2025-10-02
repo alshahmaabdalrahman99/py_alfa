fdf750f993e56083f0fa4c356176544929ed35e5
Abdalrahman <a.alshahma@student.alfa-college.nl>
2025-10-02 09:39:55 +0200

    version 0

---
+++ hello word.py
@@ -0,0 +1,31 @@
+import random
+import math
+# print('hello')
+# loop gebruiken
+# teller = 1
+# while(teller < 11):
+#     print(teller*9)
+#     antwoord = teller*9
+#     print(str(teller) + ' x 9 = ' + str(antwoord))
+#     teller += 1
+# ----------------------------------------------
+# tafel_nummer = int(input('kies een getal?'))
+# teller = 1
+# while(tafel_nummer < 11):
+#     antwoord = tafel_nummer*teller
+#     print(str(tafel_nummer) + ' x ' + str(9) + ' = ' + str(antwoord))
+#     teller += 1
+#----------------------------------------------------------------------
+# teller = 0
+# while(teller < 20):
+#      print(random.random())
+#      teller += 2
+# -----------------------------------------
+
+# teller = 0
+# while(teller < 10):
+#      print(math.floor(random.random()*6 + 1))
+#      teller += 1
+# -------------------------------------------------------
+
+                          
